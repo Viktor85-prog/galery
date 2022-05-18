34ca29eae92cf106e05bc8350cd9f456f297742e
viktor <viktor.bulkin@stremline-c.com>
2022-05-18 15:19:36 +0300

    add accordion

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
 import picruteSize from './modules/pictureSize'
+import accordion from './modules/accordion'
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -22,4 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
     calc('#size', '#material', '#options', '.promocode', '.calc-price')
     filter()
     picruteSize('.sizes-block')
+    accordion('.accordion-heading', '.accordion-block');
 });
