b0459f6d9b0b7a27e3736febdd34ca1009975708
viktor <viktor.bulkin@stremline-c.com>
2022-05-19 11:17:53 +0300

    add burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,9 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
-import picruteSize from './modules/pictureSize'
-import accordion from './modules/accordion'
+import picruteSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -17,11 +18,12 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.main-slider-item', 'vertical');
     forms();
     mask('[name="phone"]');
-    checkTextInputs('[name="name"]')
-    checkTextInputs('[name="message"]')
-    showMoreStyles('.button-styles', '#styles .row')
-    calc('#size', '#material', '#options', '.promocode', '.calc-price')
-    filter()
-    picruteSize('.sizes-block')
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
+    picruteSize('.sizes-block');
     accordion('.accordion-heading');
+    burger('.burger-menu', '.burger')
 });
